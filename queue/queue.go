26f006f5735af1c8e71dac6fcd6781fd99cf43d0
Rinat Abdullin <rinat.abdullin@gmail.com>
2014-01-17 03:51:42 -0800

    Pushing to queue works now

--- queue/queue.go
+++ queue/queue.go
@@ -1,5 +1,85 @@
-package layer
+package queue
 
 import (
-	_ "github.com/happypancake/fdb-go/fdb"
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"github.com/FoundationDB/fdb-go/fdb"
+	"github.com/FoundationDB/fdb-go/fdb/tuple"
+	"github.com/happypancake/go-layers/subspace"
 )
+
+type Queue struct {
+	Subspace       subspace.Subspace
+	HighContention bool
+	conflictedPop  subspace.Subspace
+	conflictedItem subspace.Subspace
+	queueItem      subspace.Subspace
+}
+
+func New(sub subspace.Subspace, highContention bool) Queue {
+
+	conflict := sub.Item(tuple.Tuple{"conflict"})
+	pop := sub.Item(tuple.Tuple{"pop"})
+	item := sub.Item(tuple.Tuple{"item"})
+
+	return Queue{sub, highContention, pop, conflict, item}
+}
+
+func (queue Queue) Clear(tr fdb.Transaction) {
+	tr.Clear(queue.Subspace.AsFoundationDbKey())
+}
+
+type KeyReader interface {
+	GetKey(key fdb.Selectable) fdb.FutureKey
+}
+
+// to make private
+func (queue Queue) GetNextIndex(tr KeyReader, sub subspace.Subspace) int64 {
+
+	key := tr.GetKey(fdb.LastLessThan(sub.Range(tuple.Tuple{}).End)).GetOrPanic()
+
+	if i := bytes.Compare(key, []byte(sub.Range(tuple.Tuple{}).Begin.ToFDBKey())); i < 0 {
+		return 0
+	}
+
+	if t, err := sub.Unpack(key); err != nil {
+		panic("Failed to unpack key")
+	} else {
+
+		return t[0].(int64) + 1
+
+	}
+}
+
+func (queue Queue) GetNextQueueIndex(tr fdb.Transaction) int64 {
+	return queue.GetNextIndex(tr.Snapshot(), queue.queueItem)
+}
+func (queue Queue) Push(tr fdb.Transaction, value []byte) {
+	fmt.Println("snap")
+
+	snap := tr.Snapshot()
+	fmt.Println("Get next index")
+	index := queue.GetNextIndex(snap, queue.queueItem)
+	fmt.Println("pushAt")
+	queue.pushAt(tr, value, index)
+
+}
+func (queue Queue) pushAt(tr fdb.Transaction, value []byte, index int64) {
+	fmt.Println("Pack")
+	key := queue.queueItem.Pack(tuple.Tuple{index, nextRandom()})
+	fmt.Println("Set")
+	tr.Set(fdb.Key(key), value)
+
+}
+
+func nextRandom() []byte {
+	b := make([]byte, 20)
+	if _, err := rand.Read(b); err == nil {
+		return b
+	} else {
+		fmt.Println("Panic", err)
+		panic(err)
+	}
+
+}
--- subspace/subspace.go
+++ subspace/subspace.go
@@ -1,21 +1,55 @@
 package subspace
 
 import (
-	"github.com/happypancake/fdb-go/fdb"
-	"github.com/happypancake/fdb-go/fdb/tuple"
+	"github.com/FoundationDB/fdb-go/fdb"
+	"github.com/FoundationDB/fdb-go/fdb/tuple"
 )
 
 type Subspace struct {
 	RawPrefix []byte
 }
 
-func New(rawPrefix string) {
-	return Subspace{rawPrefix}
+func New(tuple tuple.Tuple) Subspace {
+	return Subspace{tuple.Pack()}
 }
-func (space Subspace) WithTuple(tuple tuple.Tuple) Subspace {
-	return Subspace{space.RawPrefix + tuple.Pack()}
+
+func (space Subspace) WithPrefix(prefix string) Subspace {
+	buf := concat([]byte(prefix), space.RawPrefix)
+	return Subspace{buf}
 }
 
 func (space Subspace) AsFoundationDbKey() fdb.Key {
 	return fdb.Key(space.RawPrefix)
+}
+
+func (space Subspace) Item(tuple tuple.Tuple) Subspace {
+	return Subspace{concat(space.RawPrefix, tuple.Pack())}
+}
+
+func concat(old1, old2 []byte) []byte {
+	newslice := make([]byte, len(old1)+len(old2))
+	copy(newslice, old1)
+	copy(newslice[len(old1):], old2)
+	return newslice
+}
+
+func (space Subspace) Range(tuple tuple.Tuple) fdb.KeyRange {
+
+	rang := tuple.Range() // KeyRange{Key,Key}
+
+	begin := concat(space.RawPrefix, []byte(rang.BeginKey()))
+	end := concat(space.RawPrefix, []byte(rang.EndKey()))
+
+	return fdb.KeyRange{fdb.Key(begin), fdb.Key(end)}
+}
+
+func (space Subspace) Unpack(key fdb.Key) (tuple.Tuple, error) {
+
+	l := len(space.RawPrefix)
+
+	return tuple.Unpack(key[l:])
+}
+func (space Subspace) Pack(tuple tuple.Tuple) []byte {
+	return concat(space.RawPrefix, tuple.Pack())
+
 }
