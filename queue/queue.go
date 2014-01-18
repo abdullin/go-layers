12317fff8b7141bfdb26b9d4a36ba8da87efe60c
Rinat Abdullin <rinat.abdullin@gmail.com>
2014-01-17 22:44:37 -0800

    Cosmetic cleanups, switching queue methods to pointers

--- queue/queue.go
+++ queue/queue.go
@@ -26,7 +26,7 @@ func New(sub subspace.Subspace, highContention bool) Queue {
 	return Queue{sub, highContention, pop, conflict, item}
 }
 
-func (queue Queue) Clear(tr fdb.Transaction) {
+func (queue *Queue) Clear(tr fdb.Transaction) {
 	tr.Clear(queue.Subspace.AsFoundationDbKey())
 }
 
@@ -35,42 +35,34 @@ type KeyReader interface {
 }
 
 // to make private
-func (queue Queue) GetNextIndex(tr KeyReader, sub subspace.Subspace) int64 {
+func (queue *Queue) GetNextIndex(tr KeyReader, sub subspace.Subspace) int64 {
 
-	key := tr.GetKey(fdb.LastLessThan(sub.Range(tuple.Tuple{}).End)).GetOrPanic()
+	r := sub.Range(tuple.Tuple{})
 
-	if i := bytes.Compare(key, []byte(sub.Range(tuple.Tuple{}).Begin.ToFDBKey())); i < 0 {
+	key := tr.GetKey(fdb.LastLessThan(r.End)).GetOrPanic()
+
+	if i := bytes.Compare(key, []byte(r.BeginKey())); i < 0 {
 		return 0
 	}
 
 	if t, err := sub.Unpack(key); err != nil {
 		panic("Failed to unpack key")
 	} else {
-
 		return t[0].(int64) + 1
-
 	}
 }
 
-func (queue Queue) GetNextQueueIndex(tr fdb.Transaction) int64 {
+func (queue *Queue) GetNextQueueIndex(tr fdb.Transaction) int64 {
 	return queue.GetNextIndex(tr.Snapshot(), queue.queueItem)
 }
-func (queue Queue) Push(tr fdb.Transaction, value []byte) {
-	fmt.Println("snap")
-
+func (queue *Queue) Push(tr fdb.Transaction, value []byte) {
 	snap := tr.Snapshot()
-	fmt.Println("Get next index")
 	index := queue.GetNextIndex(snap, queue.queueItem)
-	fmt.Println("pushAt")
 	queue.pushAt(tr, value, index)
-
 }
-func (queue Queue) pushAt(tr fdb.Transaction, value []byte, index int64) {
-	fmt.Println("Pack")
+func (queue *Queue) pushAt(tr fdb.Transaction, value []byte, index int64) {
 	key := queue.queueItem.Pack(tuple.Tuple{index, nextRandom()})
-	fmt.Println("Set")
 	tr.Set(fdb.Key(key), value)
-
 }
 
 func nextRandom() []byte {
@@ -81,5 +73,4 @@ func nextRandom() []byte {
 		fmt.Println("Panic", err)
 		panic(err)
 	}
-
 }
